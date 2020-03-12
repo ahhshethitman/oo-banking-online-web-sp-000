28c0b482ef2bbb9b6bc9156f0dbf55304a29e37c
Hunter Sides <hunter.s.1337@gmail.com>
2020-03-12 18:33:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,8 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? == true
-     if @sender.balance > @amount && @status == "pending"
+    
+     if valid? == true && @sender.balance > @amount && @status == "pending"
        @sender.balance -= @amount
        @receiver.balance += @amount
        @status = "complete"
@@ -25,7 +25,7 @@ class Transfer
       
       return "Transaction rejected. Please check your account balance."
       end
-    end
+    
   end
 
   def reverse_transfer
